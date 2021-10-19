8198e1df8a3b17bc60c9ae23fd67df17af0a509e
Niels Brummerstedt <niels@brummerstedt.org>
2021-10-19 13:38:22 +0200

    Initial logic for Plane and Line

--- geometry/Line.hpp
+++ geometry/Line.hpp
@@ -1,3 +1,22 @@
+#ifndef EUCLID_GEOMETRY_LINE
+#define EUCLID_GEOMETRY_LINE
+
 // Defined by intersecting two points (not with point and vector)
 // A line has no direction, ie. point A and B on a line has no logical test A<B
-// That would be named either an "Infinite Ray" or an "Oriented Line"
+// That would be either an "Infinite Ray" or an "Oriented Line"
+// A line has infinite extent.
+
+#include "Point.hpp"
+
+namespace Euclid {
+
+	class Line {
+		private :
+			Point p1, p2;
+		 public:
+			constexpr Line( const Point & A, const Point & B ) : p1(A), p2(B) {};
+	};
+
+}
+
+#endif
---
+++ geometry/Plane.hpp
@@ -0,0 +1,22 @@
+#ifndef EUCLID_GEOMETRY_PLANE
+#define EUCLID_GEOMETRY_PLANE
+
+#include "Point.hpp"
+#include "Vector.hpp"
+
+// A plane is defined by an intersection point in space and its normal vector.
+// A plane spans infinitely unless bounded by a mechanism in a derived class.
+
+namespace Euclid {
+
+	class Plane {
+		protected :
+			Point 	intersect;
+			Vector 	normal;
+		public :
+			constexpr Plane( const Point & p, const Point & v ) : intersect(p), normal(v) {};
+			constexpr Plane operator - ( ) { return Ray {intersect,-normal}; };
+	};
+}
+
+#endif
